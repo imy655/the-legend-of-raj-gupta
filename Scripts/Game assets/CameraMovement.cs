2edb677797f323861b8200b1389da820b93bef46
imy655 <165296055+imy655@users.noreply.github.com>
2024-03-28 14:42:04 +0000

    Add files via upload

---
+++ Scripts/Game assets/CameraMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+
+
+public class CameraMovement : MonoBehaviour { 
+
+    public Transform target;
+    public float smoothing;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+
+
+
+    // Use this for initialization
+    void Start()
+    {}
+
+    // Update is called once per frame  
+    void LateUpdate()
+    {
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x,
+                minPosition.x, maxPosition.x);
+
+
+            targetPosition.y = Mathf.Clamp(targetPosition.y,
+                minPosition.y, maxPosition.y);
+
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+
+        }
+    }
+
+    private Vector3 RoundPosition(Vector3 position)
+    {
+        float x0ffset = position.x % .0635f;
+        if(x0ffset !=0)
+        {
+            position.x -= x0ffset;
+        }
+        float y0ffset = position.y % .0625f;
+        if(y0ffset !=0)
+        {
+            position.y -= y0ffset;
+        }
+        return position;
+    }
+}
---
+++ Scripts/PatrolLog.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolLog : log {
+
+    public Transform[] path;
+    public int currentpoint;
+    public Transform currentGoal;
+    public float roundingDistance;
+
+    public override void CheckDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius)
+        {
+            if (currentState == EnemyState.idle || currentState == EnemyState.walk && currentState != EnemyState.stagger)
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+
+                changeAnim(temp - transform.position);
+                myRigidbody.MovePosition(temp);
+                //ChangeState(EnemyState.walk);
+                anim.SetBool("wakeUp", true);
+            }
+        }
+        else if (Vector3.Distance(target.position, transform.position) > chaseRadius)
+        {
+            if (Vector3.Distance(transform.position, path[currentpoint].position) > roundingDistance)
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position, path[currentpoint].position, moveSpeed * Time.deltaTime);
+
+                changeAnim(temp - transform.position);
+                myRigidbody.MovePosition(temp);
+            }
+            else
+            {
+                ChangeGoal();
+            }
+        }
+    }
+
+
+    private void ChangeGoal()
+    {
+        if(currentpoint == path.Length - 1)
+        {
+            currentpoint = 0;
+            currentGoal = path[0];
+
+        }
+        else
+        {
+            currentpoint++;
+            currentGoal = path[currentpoint];
+        }
+    }
+
+
+}
